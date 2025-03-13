7bc4796e00b8b5351d0fbb11fa341f4fad4fb54f
AbhishekShaju <abhishekshajuts@gmail.com>
2025-03-13 10:10:33 +0530

    Update settings.py

--- mytestsite/settings.py
+++ mytestsite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-7)l96@!5-8z9hnqct3bm0snpbw37&@fzwwltzbcgb9v8_i=mpk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = '*'
 
 
 # Application definition
